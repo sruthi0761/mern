9b63407475cc761e4cb2616452e91c7e3b24412b
sruthi0761 <sruthikrishna.gp@gmail.com>
2025-01-13 16:08:43 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,8 @@
+let people = [
+    { name: "John", age: 30, occupation: "Engineer" },
+    { name: "jack", age: 48, occupation: "Teacher" },
+    { name: "Bob", age: 40, occupation: "doctor" }
+    
+]
+console.log(people[1])
+    
